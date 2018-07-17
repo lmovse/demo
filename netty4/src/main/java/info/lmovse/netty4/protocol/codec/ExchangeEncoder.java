f9262abbeab6dd7dc6462a19df70ea915ebd1c87
lmovse <hmilyssy@gmail.com>
2018-07-17 18:16:52 +0800

    modify: move random to field

--- netty4/src/main/java/info/lmovse/netty4/protocol/codec/ExchangeEncoder.java
+++ netty4/src/main/java/info/lmovse/netty4/protocol/codec/ExchangeEncoder.java
@@ -5,14 +5,14 @@ import info.lmovse.netty4.protocol.support.ProtocolMessage;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Random;
-
-import static java.time.Instant.now;
 
 public class ExchangeEncoder extends MessageToMessageEncoder<Object> {
 
     private static final short magic = (short) 0xbabe;
+    private Random random = new Random(Instant.now().toEpochMilli());
 
     @Override
     protected void encode(final ChannelHandlerContext ctx, final Object msg, final List<Object> out) {
@@ -21,7 +21,7 @@ public class ExchangeEncoder extends MessageToMessageEncoder<Object> {
             return;
         }
         byte flag = 0;
-        long requestId = new Random(now().toEpochMilli()).nextLong();
+        long requestId = random.nextLong();
         ProtocolHeader header = new ProtocolHeader(magic, flag, requestId);
         out.add(new ProtocolMessage(header, msg));
     }
