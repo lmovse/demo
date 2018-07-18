39b335e426c353c13c997c1b83dc0fa143e3c519
lmovse <hmilyssy@gmail.com>
2018-07-18 09:49:47 +0800

    modify: add reconnect mechanism

--- netty4/src/main/java/info/lmovse/netty4/protocol/client/ProtocolClient.java
+++ netty4/src/main/java/info/lmovse/netty4/protocol/client/ProtocolClient.java
@@ -10,6 +10,7 @@ import info.lmovse.netty4.protocol.codec.TransportDecoder;
 import info.lmovse.netty4.protocol.codec.TransportEncoder;
 import info.lmovse.netty4.protocol.serialization.KryoSerialization;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -18,36 +19,54 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ProtocolClient {
 
+    private static final NioEventLoopGroup WORKER = new NioEventLoopGroup();
+    private final Bootstrap bootstrap = new Bootstrap();
+    private final ProtocolCodec codec = new ProtocolCodec(new KryoSerialization());
+    private ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
     public static void main(String[] args) {
-        NioEventLoopGroup worker = new NioEventLoopGroup();
-        ProtocolCodec codec = new ProtocolCodec(new KryoSerialization());
-        Bootstrap bootstrap = new Bootstrap();
+        new ProtocolClient().connect(new InetSocketAddress(7070));
+    }
+
+    public void connect(final SocketAddress address) {
         try {
-            ChannelFuture channelFuture =
-                    bootstrap.group(worker)
-                            .channel(NioSocketChannel.class)
-                            .handler(new ChannelInitializer<SocketChannel>() {
-                                @Override
-                                protected void initChannel(final SocketChannel ch) {
-                                    ch.pipeline()
-                                            .addLast("transport-decoder", new TransportDecoder(codec))
-                                            .addLast("exchange-decoder", new ExchangeDecoder())
-                                            .addLast("transport-encoder", new TransportEncoder(codec))
-                                            .addLast("auth-handler", new AuthRequestHandler())
-                                            .addLast("heartbeat-handler", new HeartBeatRequestHandler())
-                                            .addLast("exchange-encoder", new ExchangeEncoder())
-                                            .addLast("handler", new ClientHandler());
-                                }
-                            })
-                            .option(ChannelOption.SO_KEEPALIVE, true)
-                            .connect(new InetSocketAddress("localhost", 7070))
-                            .sync();
+            // basic config
+            bootstrap.group(WORKER)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000);
+
+            // config handlers
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(final SocketChannel ch) {
+                    ch.pipeline()
+                            .addLast("transport-decoder", new TransportDecoder(codec))
+                            .addLast("exchange-decoder", new ExchangeDecoder())
+                            .addLast("transport-encoder", new TransportEncoder(codec))
+                            .addLast("auth-handler", new AuthRequestHandler())
+                            .addLast("heartbeat-handler", new HeartBeatRequestHandler())
+                            .addLast("exchange-encoder", new ExchangeEncoder())
+                            .addLast("handler", new ClientHandler());
+                }
+            });
+
+            // connect server
+            ChannelFuture channelFuture = bootstrap.connect(address).sync();
             channelFuture.channel().closeFuture().sync();
         } catch (InterruptedException e) {
-            worker.shutdownGracefully();
+//            can't shutdown nioEventGroup
+//            worker.shutdownGracefully();
+            executor.schedule(() -> connect(address), 1000, TimeUnit.MILLISECONDS);
         }
     }
 }
