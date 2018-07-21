6ef3707b59a283d25b842c9fc446557268b6da9a
lmovse <hmilyssy@gmail.com>
2018-07-21 10:02:24 +0800

    etc: rename

--- spring-cloud/cloud-registry/src/main/java/info/lmovse/springcloud/registry/CloudRegistry.java
+++ spring-cloud/cloud-registry/src/main/java/info/lmovse/springcloud/registry/CloudRegistry.java
@@ -1,4 +1,4 @@
-package info.lmovse.registry;
+package info.lmovse.springcloud.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
